753f7c3356f84b5de21df20b4a8c3de8903dbaad
Thomas Lamb <tclamb@ufl.edu>
2013-08-02 22:56:48 -0400

    a couple extra examples

---
+++ examples/boost_helloWorld.cpp
@@ -0,0 +1,21 @@
+#include "../Signal.hpp"
+#include <iostream>
+
+struct HelloWorld
+{
+    void operator()() const
+    {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
+
+int main() {
+    Signal<void()> sig;
+
+    HelloWorld hello;
+    sig.connect(hello);
+
+    sig();
+
+    return 0;
+}
---
+++ examples/parameters.cpp
@@ -0,0 +1,33 @@
+#include "../Signal.hpp"
+#include <iostream>
+
+int main() {
+    Signal<int(int)> mySignal;
+
+    std::cout << mySignal(30) << std::endl;
+
+    mySignal.connect([](int a) { return a; });
+    mySignal.connect([](int a) { std::cout << a << std::endl; return 0; });
+
+    std::cout << mySignal(15) << std::endl;
+
+    /* Output:
+     *     0
+     *     15
+     *     0
+     */
+
+    Signal<void(float)> mySignal2;
+
+    mySignal2.connect([](float a) { std::cout << 0 << std::endl; });
+    mySignal2.connect([](float a) { std::cout << a << std::endl; });
+
+    mySignal2(15);
+
+    /* Output:
+     *     0
+     *     15
+     */
+
+    return 0;
+}
